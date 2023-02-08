09f7f39fb7e07b033c00530db1565aca2651b1cc
omarali <o.ali94@gmail.com>
2023-02-08 23:06:04 +0000

    using @cyclic.sh/session-store npm package to reconfigure express session

--- config/session.config.js
+++ config/session.config.js
@@ -1,20 +1,44 @@
 const session = require("express-session");
+const { CyclicSessionStore } = require("@cyclic.sh/session-store");
+
+// const sessionFunction = (app) => {
+//   app.set("trust proxy", 1);
+//   app.use(
+//     session({
+//       secret: process.env.SESS_SECRET,
+//       resave: true,
+//       saveUninitialized: false,
+//       cookie: {
+//         sameSite: "none",
+//         secure: true,
+//         httpOnly: true,
+//         maxAge: 6000000, // 60 * 1000 ms === 1 min
+//       },
+//     })
+//   );
+// };
+
+const options = {
+  table: {
+    name: 'cycles',
+  }
+};
+
+
 
 const sessionFunction = (app) => {
-  app.set("trust proxy", 1);
   app.use(
     session({
-      secret: process.env.SESS_SECRET,
-      resave: true,
-      saveUninitialized: false,
-      cookie: {
-        sameSite: "none",
-        secure: true,
-        httpOnly: true,
-        maxAge: 6000000, // 60 * 1000 ms === 1 min
-      },
+      store: new CyclicSessionStore(options),
+      
     })
   );
 };
 
 module.exports = sessionFunction;
+
+
+
+
+const app = express();
+

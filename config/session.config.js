95cc90484d9791e3ac76bced162f9d334b84c6ce
omarali <o.ali94@gmail.com>
2023-02-09 00:55:03 +0000

    saveUninitialized: true

--- config/session.config.js
+++ config/session.config.js
@@ -1,44 +1,38 @@
 const session = require("express-session");
 const { CyclicSessionStore } = require("@cyclic.sh/session-store");
 
-// const sessionFunction = (app) => {
-//   app.set("trust proxy", 1);
-//   app.use(
-//     session({
-//       secret: process.env.SESS_SECRET,
-//       resave: true,
-//       saveUninitialized: false,
-//       cookie: {
-//         sameSite: "none",
-//         secure: true,
-//         httpOnly: true,
-//         maxAge: 6000000, // 60 * 1000 ms === 1 min
-//       },
-//     })
-//   );
-// };
-
-const options = {
-  table: {
-    name: 'cycles',
-  }
-};
-
-
-
 const sessionFunction = (app) => {
+  app.set("trust proxy", 1);
   app.use(
     session({
-      store: new CyclicSessionStore(options),
-      
+      secret: process.env.SESS_SECRET,
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+        maxAge: 6000000, // 60 * 1000 ms === 1 min
+      },
     })
   );
 };
 
+// const options = {
+//   table: {
+//     name: 'cycles',
+//   }
+// };
+
+// const sessionFunction = (app) => {
+//   app.use(
+//     session({
+//       store: new CyclicSessionStore(options),
+
+//     })
+//   );
+// };
+
 module.exports = sessionFunction;
 
-
-
-
 const app = express();
-
